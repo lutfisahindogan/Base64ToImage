d772941e0db79c8cd149fff155e7d3cc7e9b6913
Lütfi Şahindoğan <lutfi.sahindogan@simsoft.com.tr>
2016-10-05 12:46:40 +0300

    Project Created

---
+++ Base64ToImage/Base64ToImage/Base64ToImage/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Base64ToImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pictureBox1.Location = new System.Drawing.Point(348, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(583, 843);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(308, 843);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(314, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 331);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Base64Data";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(314, 379);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(28, 155);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(314, 540);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(28, 158);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(317, 704);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(28, 127);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(931, 843);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Base64ToImage/Base64ToImage/Base64ToImage/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Base64ToImage
+{
+    public partial class Form1 : Form
+    {
+        public int CropPixel { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+            CropPixel = 0;
+            label1.Text = "B\nA\nS\nE\n\n6\n4\n\nC\no\nd\ne\n";
+            button1.Text = "C\nO\nN\nV\nE\nR\nT\n";
+            button2.Text = "C\nL\nE\nA\nR\n";
+            button3.Text = "C\nR\nO\nP\n";
+            button1.Click += Button1_Click;
+            button2.Click += Button2_Click;
+            button3.Click += Button3_Click;
+            textBox1.MaxLength = Int32.MaxValue;
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            CropPixel += 5;
+            string @base = null;
+            @base = textBox1.Text.Replace("\r", "");
+            Bitmap bmp = newBitmapFromBase64(@base);
+
+
+            Rectangle cropRect = new Rectangle(0, 0, bmp.Width, bmp.Height - CropPixel);
+            Bitmap src = bmp;
+            Bitmap target = new Bitmap(cropRect.Width, cropRect.Height);
+
+            using (Graphics g = Graphics.FromImage(target))
+            {
+                g.DrawImage(src, new Rectangle(0, 0, target.Width, target.Height),
+                                 cropRect,
+                                 GraphicsUnit.Pixel);
+            }
+            bmp = target;
+            pictureBox1.Image = bmp;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            CropPixel = 0;
+            string @base = null;
+            @base = textBox1.Text.Replace("\r", "");
+            Bitmap bmp = newBitmapFromBase64(@base);
+            pictureBox1.Image = bmp;
+        }
+        public Bitmap newBitmapFromBase64(string base64)
+        {
+            byte[] array = Convert.FromBase64String(base64);
+            Bitmap result;
+            using (MemoryStream memoryStream = new MemoryStream(array, 0, array.Length))
+            {
+                Bitmap bitmap = new Bitmap(memoryStream, false);
+                result = bitmap;
+            }
+            return result;
+        }
+    }
+}
